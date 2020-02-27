62c884f2889b2ec00ea4ca299bb00d1eee7ec8fc
Scott Bergeron <scott.bergeron1991@gmail.com>
2020-02-26 20:43:16 -0500

    Lesson 159

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,19 @@ import classes from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = (props) => {
+    const ingredients = Object.keys(props.ingredients).map(
+        (ingredientName) => {
+            return [...Array(props.ingredients[ingredientName])].map(
+                (_, index) => {
+                    return <BurgerIngredient key={ingredientName + index} type={ingredientName} />
+                }
+            );
+        }
+    );
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {ingredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
